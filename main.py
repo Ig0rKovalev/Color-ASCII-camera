2f1f54c545cee0aef5038fda9b985a273921b50d
Igor Kovalev <ig0rkovalev@mail.ru>
2022-03-24 12:50:14 +0700

    Project optimization

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-import cv2
+from cv2 import (cvtColor, COLOR_RGB2BGR, imshow, VideoCapture,
+                 waitKey, INTER_NEAREST, resize)
 from PIL import ImageFont, ImageDraw, Image
-import numpy as np
+from numpy import array
+from os.path import dirname, abspath
 
 
 DENSITY = ' .",:;!~+-cba*#W&8@'
 LEN_DENSITY = 256 / len(DENSITY)
+FONT = ImageFont.truetype(dirname(abspath(__file__)) + "\courier.ttf", size=9)
 
 
-def drow(img, height, width):
-    img_new = Image.new("RGBA", (height, width), 'black')
-
+def drow(img):
+    img_new = Image.new("RGBA", (600, 600), 'black')
     draw = ImageDraw.Draw(img_new)
-    font = ImageFont.truetype("courier.ttf", 7)
 
-    for i in range(0, width, 5):
-        for j in range(0, height, 5):
+    for i in range(0, 601, 7):
+        for j in range(0, 601, 7):
             b, g, r = img[i, j]
             b = int(b)
             g = int(g)
             r = int(r)
-            pixelIndex = int((b + g + r) // 3 // LEN_DENSITY)
+            pixelIndex = int(((b + g + r) // 3) // LEN_DENSITY)
             if int((((b + g + r) // 3) % LEN_DENSITY)) == 0 and pixelIndex != 0:
-                draw.text((j, i), DENSITY[pixelIndex-1], fill=(r, g, b), font=font)
+                draw.text((j, i), DENSITY[pixelIndex-1], fill=(r, g, b), font=FONT)
             else:
-                draw.text((j, i), DENSITY[pixelIndex], fill=(r, g, b), font=font)
-                
-    cv2_im_processed = cv2.cvtColor(np.array(img_new), cv2.COLOR_RGB2BGR)
-    cv2.imshow("main", cv2_im_processed)
+                draw.text((j, i), DENSITY[pixelIndex], fill=(r, g, b), font=FONT)
+
+    cv2_im_processed = cvtColor(array(img_new), COLOR_RGB2BGR)
+    imshow("ASCII Video", cv2_im_processed)
 
 
 def main():
-    cap = cv2.VideoCapture(0)
+    cap = VideoCapture(0)
 
     while True:
         ret, img = cap.read()
-        img = cv2.resize(img, (600, 600), interpolation=cv2.INTER_NEAREST)
-        drow(img, 600, 600)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        img = resize(img, (600, 600), interpolation=INTER_NEAREST)
+        drow(img)
+        if waitKey(1) & 0xFF == ord('q'):
             return
 
 
